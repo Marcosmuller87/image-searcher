c5d8fc9dcc59aaba722918487fcb7a31cee73eca
Marcos Muller <marcos.muller@magmati.com.br>
2023-04-03 15:13:13 -0300

    Basic structure of the project finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ImageSearchComponent } from './components/image-search/image-search.component';
+import { ImageListComponent } from './components/image-list/image-list.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SpinnerComponent } from './shared/spinner/spinner.component';
+import { ErrorComponent } from './shared/error/error.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImageSearchComponent,
+    ImageListComponent,
+    NavbarComponent,
+    SpinnerComponent,
+    ErrorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
